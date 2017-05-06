37c351fcc2df21dc842e7b9f6b283f7b7e72a128
Giulia <giulia.gdep@gmail.com>
2017-05-06 11:37:27 +0200

    Script per recuperare entita tagme e salvarle nel db

--- Processor/indeed_cloud.py
+++ Processor/indeed_cloud.py
@@ -1,21 +1,62 @@
 import os
-from processhtml import *
 from tagcloud import *
 from collections import Counter
 from tagme import *
+import mysql.connector
 import pprint
 
+
+min_rho= 0.2
+
+db=mysql.connector.connect(host="localhost",user="",
+                  passwd="",db="JobTest")
+
+cursor = db.cursor(buffered=True)
+query_select = "SELECT id,jobtitle,testo FROM lavori_all WHERE testo IS NOT NULL AND entity_processed=0 LIMIT 1"
+cursor.execute(query_select)
+jobs = cursor.fetchall()
+
+
+
+query_insert = "INSERT INTO entities (title,wikipedia_id,lavoro_id) VALUES (%s,%s,%s)"
+query_update = "UPDATE lavori_all SET entity_processed=%s WHERE id=%s"
+
+
+print len(jobs)
 
 
 entities = []
 
-for filename in os.listdir(PAGES_DIR):
-    text = html_to_text(os.path.join(PAGES_DIR, filename))
-    if text:
-        text_entities = get_entities(query_tagme(text), 0.2)
-        print text_entities
-        entities += text_entities
+for job in jobs:
+    testo = job[2]
+    print testo
+    #print testo
+    if testo:
+        qr=query_tagme(testo)
+        qr_ann=qr['annotations']
+        print "annotations"
+        ann = [a for a in qr_ann if float(a["rho"]) > min_rho]
+
+        for a in ann:
+            data_insert=(a['title'], a['id'], job[0])
+            print data_insert
+            cursor.execute(query_insert, data_insert)
+
+        data_update = (1,job[0])
+        cursor.execute(query_update,data_update)
+
+        db.commit()
+
+        #for parola in text_entities:
+
+
+        #entities += text_entities
+        #entities.append(testo)
 
 c = Counter(entities)
+print "Parole piu frequenti"
 print c.most_common(100)
-generate_tag_cloud(c.most_common(100), "corriere_entity.png")
+#generate_tag_cloud(c.most_common(100), "lavoro.png")
+
+cursor.close()
+db.close()
